c1a812b6d77b008527c3182a12885b02661cff4a
ylabate <12196@holbertonstudents.com>
2025-12-16 09:21:56 +0000

    fix: enhance command path handling and subprocess execution flow

--- subprocess.c
+++ subprocess.c
@@ -5,6 +5,7 @@ int exec_subprocess(char *path, char **arguments)
 	pid_t pid_proc;
 	int status;
 	extern char **environ;
+	int exit_code;
 
 	if (path == "exit")
 		exit(EXIT_SUCCESS);
@@ -12,10 +13,18 @@ int exec_subprocess(char *path, char **arguments)
 	{
 		pid_proc = fork();
 		if (pid_proc != 0)
+		{
 			waitpid(pid_proc, &status, 0);
+			if (WIFEXITED(status))
+			{
+				exit_code = WEXITSTATUS(status);
+			}
+		}
 		else
 		{
 			execve(path, arguments, environ);
+			perror("execve");
+			exit(1);
 		}
 	}
 	return (0);
