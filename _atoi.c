afd637e5fc12f480551f9067e4e80e6aedd395c9
ylabate <12196@holbertonstudents.com>
2025-12-18 16:20:30 +0000

    fix: refactor _atoi function for improved string to integer conversion

--- _atoi.c
+++ _atoi.c
@@ -1,35 +1,33 @@
 #include "main.h"
+
 /**
  * _atoi - converts a string to an integer
  * @s: pointer to the string
  *
  * Return: the integer value
  */
-
 int _atoi(char *s)
 {
-	unsigned int o = 0;
-	int i;
-	int ii = 0;
-	int negative = 1;
-	int n;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-	for (i = 0; ii == 0; i++)
+	if (!s)
+		return (0);
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '\r')
+		i++;
+	if (s[i] == '-')
 	{
-		if ((s[i] >= 48 && s[i] <= 57))
-		{
-			for (; s[i + ii] >= 48 && s[i + ii] <= 57; ii++)
-			{
-				o = (s[i + ii] - 48 + (o * 10));
-			}
-		}
-		else if (s[i] == '\0')
-			return (0);
+		sign = -1;
+		i++;
 	}
-	for (n = 0; s[n] != 'n'; n++)
-		if (s[n] == 45)
-			negative = -negative;
-	if (negative < 0)
-		return (-o);
-	return (o);
+	else if (s[i] == '+')
+		i++;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (result * sign);
 }
