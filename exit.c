2818844b37b225fff1d9b43c6c6374cb039a6ff2
ylabate <12196@holbertonstudents.com>
2025-12-19 10:45:47 +0000

    fix: improve exit command error handling for illegal numbers

--- exit.c
+++ exit.c
@@ -6,21 +6,29 @@
  * @end: pointer to flag indicating shell should end
  * @token: array of command tokens
  * @count: command execution count for error messages
+ * @av: array of argument vectors (program name)
  *
  * Return: 1 if exit command was processed, 0 otherwise
  */
 int __exit(int *exit_code, int *end, char **token, int count, char **av)
 {
-	int ext_code = 0;
+	int ext_code = 0, i = 0;
 
 	if (token[1])
+	{
 		ext_code = _atoi(token[1]);
-
+		while (token[1][i])
+		{
+			if (token[1][i] > '9' || token[1][i] < '0')
+				ext_code = -1;
+			i++;
+		}
+	}
 	if (strcmp(token[0], "exit") == 0)
 	{
 		if (ext_code < 0)
 		{
-			fprintf(stderr, "%s: %d: exit: Illegal number: %d\n", av[0], count, ext_code);
+			fprintf(stderr, "%s: %d: exit: Illegal number: %s\n", av[0], count, token[1]);
 			*exit_code = 2;
 		}
 		else
