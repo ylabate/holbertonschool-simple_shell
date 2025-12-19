767f0e0e3d68cc2007dc0303d650bd29bad50564
theocaulet <caulet.theo@gmail.com>
2025-12-19 09:51:23 +0100

    feat: implement built_in_command function to check for local commands

---
+++ built_in_command.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * built_in_command - checks if the command is a local command
+ * @token: array of command and its arguments
+ * @envp: environment variables
+ * Return: 1 if local command, 0 otherwise
+ */
+int built_in_command(char **token, char **envp)
+{
+	int i = 0;
+	function_t func[] = {
+		{"env", shell_env, NULL},
+		{NULL, NULL, NULL}};
+
+	func[0].arguments = envp;
+	while (func[i].name)
+	{
+		if (strcmp(token[0], func[i].name) == 0)
+			return (func[i].function(func[i].arguments));
+		i++;
+	}
+	return (256);
+}
